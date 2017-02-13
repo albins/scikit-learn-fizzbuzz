998da9988d4b505b4a58671d8a4f71758f4ebb1f
Albin Stjerna <albin.stjerna@gmail.com>
2017-02-13 11:05:07 +0100

    Enable providing number of features on command line

--- fizzbuzz_advanced.py
+++ fizzbuzz_advanced.py
@@ -9,15 +9,15 @@ FIZZ = 1
 BUZZ = 2
 FIZZBUZZ = 3
 
-def num_div_1_to_9(n):
-    return [int((n % x) == 0) for x in range(1,10)]
+def num_div_1_to_k(n, k):
+    return [int((n % x) == 0) for x in range(1,k)]
 
 
-def make_training_set(size):
+def make_training_set(height, width):
     data = []
     targets = []
 
-    for n in range (1, size+1):
+    for n in range (1, height+1):
         result = fizzbuzz(n)
 
         if result == "Fizz":
@@ -29,15 +29,15 @@ def make_training_set(size):
         else:
             actual_result = NUM
 
-        features = num_div_1_to_9(n)
+        features = num_div_1_to_k(n, width)
         data.append(features)
         targets.append(actual_result)
 
     return data, targets
 
 
-def fizzbuzz_ml(clf, n):
-    prediction, = clf.predict([num_div_1_to_9(n)])
+def fizzbuzz_ml(clf, n, width):
+    prediction, = clf.predict([num_div_1_to_k(n, width)])
 
     if prediction == NUM:
         answer = n
@@ -54,20 +54,27 @@ def fizzbuzz_ml(clf, n):
 
 if __name__ == '__main__':
 
+    if len(sys.argv) < 4:
+        print("Usage: {} <number of tests> <training set height> <num features/training set width>"
+              .format(sys.argv[0]))
+        exit(1)
+
     N = (int(sys.argv[1]) + 1)
     training_size = int(sys.argv[2])
+    num_features = int(sys.argv[3])
 
-    X, Y = make_training_set(training_size)
-    clf = RandomForestClassifier(n_estimators=10)
+    X, Y = make_training_set(training_size, num_features)
+    clf = RandomForestClassifier()
     clf = clf.fit(X, Y)
 
-    print("Trained on integers [1,{}]".format(training_size))
+    print("Trained on integers [1,{}], with {} mod-values."
+          .format(training_size, num_features))
 
     success = 0
     failures = 0
     for n in range(1, N):
         #print("{}: {}/{}".format(n, fizzbuzz_ml(clf, n), fizzbuzz(n)))
-        ml_result = fizzbuzz_ml(clf, n)
+        ml_result = fizzbuzz_ml(clf, n, num_features)
         expected_result = fizzbuzz(n)
         if ml_result == expected_result:
             success += 1
